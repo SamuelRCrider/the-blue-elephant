bdbfb0b0b7b84b0e992ddcfd7609075ca7a031b9
Samuel Crider <samuelcrider@Samuels-MacBook-Pro.local>
2023-08-17 21:20:24 -0400

    turned off caching

--- app.py
+++ app.py
@@ -18,6 +18,16 @@ con = sqlite3.connect("blue_elephant.db")
 cur = con.cursor()
 
 
+# turn off caching
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 # app routes
 @app.route("/", methods=["GET", "POST"])
 def index():
